3c74fb36984c699fea3e0fe87a2f7322120b4b97
Far506 <98295031+Far506@users.noreply.github.com>
2022-11-23 07:28:02 -0800

    Add files via upload

---
+++ School-management-system-main/student/View/dashboard.php
@@ -0,0 +1,26 @@
+<?php
+$title= "Dashboard";
+include('header.php');
+?>
+
+<?php include('sideBar.php'); ?> 
+        <td>
+          <h1>Welcome to student page</h1>
+        </td>
+      </tr>
+
+    <?php include('footer.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1 {
+  background-color: DodgerBlue;
+}
+</style>
+</head>
+<body>
+
+</body>
+</html>
---
+++ School-management-system-main/student/View/footer.php
@@ -0,0 +1,8 @@
+<tr>
+        <td align="center" colspan="2" ></td>
+      </tr>
+
+    </table>
+
+  </body>
+</html>
